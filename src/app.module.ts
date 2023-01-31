bf70c317d3b53d7058be6a77a493f4e7a6ee8876
Nurettin Şen <nurie487@gmail.com>
2023-01-31 17:37:47 +0300

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MessagesModule } from 'src/modules/messages/messages.module';
+import { ReportsModule } from './modules/reports/reports.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -8,6 +10,8 @@ import { MessagesModule } from 'src/modules/messages/messages.module';
       isGlobal: true,
     }),
     MessagesModule,
-  ],
+    ReportsModule,
+    UsersModule,
+  ], // burda providers ve controller yok cunku app.controllers ve app.services'ı yarattıydı sıldık onları gerek yok onlara
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,5 @@ async function bootstrap() {
   await app.listen(5000);
 }
 bootstrap();
+
+// typeorm bır orm aracı sequilize ve mongoose gibi. en onemlı ozellıgı ornegın mongodb sqlite3  yada pg yukledıgınde nnpm i pg dıyerek daha sonra npm i typeorm dersın. bu typeorm dıger verıtabanları ıcıınde orm olarak kullanabılrısn
