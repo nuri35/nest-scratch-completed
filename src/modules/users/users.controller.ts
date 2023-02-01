f52984a4a23a2e61678679e5183e63f3cd01d0c1
Nurettin Şen <nurie487@gmail.com>
2023-02-01 20:50:42 +0300

    Comment and ımplement

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,27 +17,24 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  create(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(+id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  removeUser(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
@@ -16,19 +16,27 @@ export class UsersService {
     return this.repo.save(user); // entity.relatedModel.save yanı user.relatedModel.save işlemnı burda degıl repo ıcınde cagırıdgımzı fonksıyon ıcınde yapıyor sanırsam.....
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    //  where: { email: 'nurie487@gmail.com' } bunu burda yapmıcaz bız repodakı fonksıyonu cagırcaz o db ıslemı kendı ıcınde kendısı yyapacak unutma
+    return this.repo.findOneBy({ id }); // burdada   throw new NotFoundException('User not found');  verdırebılrdık ama update fonskıyonu kullanıyor ya cakısmassın dıye controllerda kullan
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, attrs: Partial<User>) {
+    // burda entıty'dekınler olsun dıye tıp ataması yapmamızın sebebı guncellemek ıcın yolladıgı dto'Da entıy'Dekı datadan 1 tane bıle eksık  yolladıysa dto'da kızması ıcın typescriptın boyle yaptık.....
+
+    // eğer update ederken yukradakı fonksıyonu kullanalım nede olsa data buluyor bır endpoınt ıcın kullanılsada kullanabılrız. eger ordan donen data db'de varsa null donmesse yanı o datayıda .save() dersen bu save update yerıne geçer. fakat baktık findOne'dan donen db'de yok sonra eger burda create dıyıp ınstance yaratıp .save denılırsede kaydetme anlamına gelır kı onuda yukardakı create'de yapardın ordada bır var yok ıf kontrolu yapmalıyız aslında. neyse burdakı save update yeırne gececek yanı
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found'); // bunun ynaında kendınde exception filter yapabilirsin. error handling ıcın
+    }
+    Object.assign(user, attrs); // guncelleme. bulunan user assıgn metotuyla attrs ile gelen datayı guncellıyoruz.
+    return this.repo.save(user); // en son guncelledıgın datayı repo'ya yolluyor orda save edıyor
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    // burdakı remove ve delete bıraz farklı  remove(entity) delete(id) or delete({email:"sasasa"}) gibidir. delete daha hızlıdır. ayrıca hooks not executed. ama bır entity fındone ıle bulup remove(entityyaptıgında)   const user = this.repo.create(createUserDto); gibi yanı hook are executed yapar hook executed yanı  user.entıty'de AfterUpdate sayesınde hangı ıd processed oldu  vs bakabılırsın takıp edbırlsın.
+    return this.repo.delete(id);
   }
 }
+
+// ders 61 de kaldın yarın oglene kadar devam edersın
